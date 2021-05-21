afa58f7a6a1d12e25ab5b96d5cfbbf9933df4a6c
knagadevara <vnsk.1991@gmail.com>
2021-05-21 21:52:44 +0530

    general variable decleration

--- 03Prog/main.cpp
+++ 03Prog/main.cpp
@@ -1,7 +1,20 @@
-#include <stdio.h>
+#include <iostream>
 
-int main(int argc, char **argv)
-{
-	printf("hello world\n");
-	return 0;
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main() {
+	
+	cout << "Enter the width of the room: ";
+	int room_width {0} ;
+	cin >> room_width ;
+	
+	cout << "Enter the length of the room: ";
+	int room_length {0} ;
+	cin >> room_length ;
+	
+	cout << "AREA of the room is: " << room_length * room_width << endl;
+	return 0  ;
+	
 }
