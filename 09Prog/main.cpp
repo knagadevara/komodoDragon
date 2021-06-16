00f383b5ac4bf3e3245607ef9691acf7747db9b0
knagadevara <vnsk.1991@gmail.com>
2021-06-15 20:34:52 -0400

    howmuch i have

---
+++ 09Prog/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+const double currencyDollar[5] {100,50,25,10,5};
+// const double FullDollar {100};
+// const double HalfDollar {50};
+// const double QuaterDollar {25};
+// const double TenthOfDollar {10};
+// const double HalfOfTenth {5};
+
+
+
+int main()
+    {
+        return 0;
+    }
