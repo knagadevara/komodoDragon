4d92056c10cfc02fd928aa8fbf4fa7bf2fdc5e0e
knagadevara <vnsk.1991@gmail.com>
2021-08-01 09:57:29 +0530

    restructure

--- 02OO/02Prog/src/Player.cpp
+++ 02OO/02Prog/src/Player.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#include "Player.h"
+#include "../include/Player.h"
 
 void Player::set_player_name(std::string first_name, std::string last_name)
     {
@@ -35,27 +35,11 @@ bool Player::is_dead(double health)
             return false;        
     }
 
-void Player::set_player_weight(double weight)
-    {
-        this->weight = weight;
-    }
+void Player::set_player_weight(double weight) { this->weight = weight; }
+double Player::get_player_weight() { return this->weight; }
 
-double Player::get_player_weight()
-    {
-        std::cout << "Player Weight: " << this->weight << std::endl;
-        return this->weight;
-    }
-
-void Player::set_player_height(double height)
-    {
-        this->height = height;
-    }
-
-double Player::get_player_height()
-    {
-        std::cout << "Player Height:" << this->height << std::endl;
-        return this->height;
-    }
+void Player::set_player_height(double height) { this->height = height; }
+double Player::get_player_height() { return this->height; }
 
  Player::Player()
         : weight{0.0} , height{0.0} , first_name{""} , last_name{""}
@@ -64,19 +48,21 @@ double Player::get_player_height()
     }
 
  Player::Player(std::string first_name, std::string last_name)
-        : weight{0.0} , height{0.0}  
+        : first_name{first_name} , last_name{last_name} , weight{0.0} , height{0.0}
     {
-        this->set_player_name(first_name, last_name);
         std::cout << "Created! " 
                   << "Name: "   << this->get_player_name()
                   << std::endl;
     }
 
  Player::Player(std::string first_name, std::string last_name , double weight , double height)
+        : first_name{first_name} , last_name{last_name} , weight{weight} , height{height}
     {
-        this->set_player_name(first_name, last_name);
-        this->set_player_weight(weight);
-        this->set_player_height(height);
+        std::cout << "Created! " 
+                  << "Name: "   << this->get_player_name()
+                  << "Weight: " << this->get_player_weight()
+                  << "Height: " << this->get_player_height()
+                  << std::endl;
     }
 
  Player::~Player()
--- 02OO/02Prog/src/main.cpp
+++ 02OO/02Prog/src/main.cpp
@@ -1,4 +1,4 @@
-#include "Player.h"
+#include "../include/Player.h"
 
 int main(void)
     {
